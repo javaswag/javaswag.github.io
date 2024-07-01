16ffb9b12ef5287592f1c9f48c158c44c48f96a8
volyx <volyihin@gmail.com>
2024-07-01 16:15:29 +0100

    add sample rate

--- audio/main.go
+++ audio/main.go
@@ -8,6 +8,12 @@ import (
 	"github.com/hajimehoshi/go-mp3"
 )
 
+const (
+	gomp3NumChannels   = 2
+	gomp3Precision     = 2
+	gomp3BytesPerFrame = gomp3NumChannels * gomp3Precision
+)
+
 func main() {
 	fmt.Println("Open: ", os.Args[1])
 	f, err := os.Open(os.Args[1])
@@ -23,7 +29,7 @@ func main() {
 
 	fmt.Printf("SampleRate %d\n", d.SampleRate())
 	// 134,407,456
-	fmt.Printf("Length %d bytes\n", d.Length())
+	fmt.Printf("Length %d bytes\n", d.Length()/gomp3BytesPerFrame)
 	// fmt.Printf("Length %d Mb\n", )
 
 }
